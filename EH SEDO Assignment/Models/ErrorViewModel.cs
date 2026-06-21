29930eb7117c8f9bcd7642308cf055a7ed6002aa
elaine <elainehenry1712@gmail.com>
2026-06-21 11:32:19 +0100

    Add project files.

---
+++ EH SEDO Assignment/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EH_SEDO_Assignment.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
